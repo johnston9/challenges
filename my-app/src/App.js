f64496132809891fc03bc23f2c4462f750083774
johnston9 <davejohnston9@gmail.com>
2021-10-11 23:33:52 +0000

    search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 // import NavBarSimple from "./components/NavBarSimple";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
+// import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarForm />
+      {/* <NavBarForm />
       <Sidebar />
-      <Content />
+      <Content /> */}
+      <SearchBar />
     </div>
   );
 }
